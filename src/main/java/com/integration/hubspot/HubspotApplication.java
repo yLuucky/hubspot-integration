ea30e35a19454aa1cc1e516586ebd77c379cd067
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-03-15 08:22:15 -0300

    initial commit

---
+++ src/main/java/com/integration/hubspot/HubspotApplication.java
@@ -0,0 +1,13 @@
+package com.integration.hubspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integration/hubspot/HubspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.hubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
