3cdb577043a4b3017bf1b55488750d45cb25c859
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-03-15 08:23:46 -0300

    feat: added @Async Annotation

--- src/main/java/com/integration/hubspot/HubspotApplication.java
+++ src/main/java/com/integration/hubspot/HubspotApplication.java
@@ -2,7 +2,9 @@ package com.integration.hubspot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
 
+@Async
 @SpringBootApplication
 public class HubspotApplication {
 
