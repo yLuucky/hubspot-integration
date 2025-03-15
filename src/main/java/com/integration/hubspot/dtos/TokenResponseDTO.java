73ea76c50657ae0d099d5396490889a241d2dde5
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-03-15 08:24:03 -0300

    feat: created TokenResponseDTO

---
+++ src/main/java/com/integration/hubspot/dtos/TokenResponseDTO.java
@@ -0,0 +1,23 @@
+package com.integration.hubspot.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponseDTO {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+}
